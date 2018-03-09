c10891b5e165806b746d69eb95154e4536e71e06
songy23 <songya@google.com>
2018-03-09 14:08:58 -0800

    Add Java gRPC interop test client and server.

--- integration/go-http-client/client_test.go
+++ integration/go-http-client/client_test.go
@@ -34,7 +34,7 @@ import (
 	"go.opencensus.io/trace"
 	"go.opencensus.io/trace/propagation"
 
-	pb "github.com/census-instrumentation/opencensus-experiments/integration/proto"
+	pb "github.com/census-instrumentation/opencensus-experiments/integration/src/main/proto"
 )
 
 var setups = []struct {
