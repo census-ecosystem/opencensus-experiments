32634770df3e5c2f664d372875455df13f6c2167
Emmanuel T Odeke <odeke@ualberta.ca>
2018-05-23 16:19:15 -0700

    integration/go: fix import paths that got changed after rename (#40)
    This repository was moved from
        `census-instrumentation/opencensus-experiments`
    to
        `census-ecosystem/opencensus-experiments`
    
    but the rename wasn't applied to the code inside. I just
    noticed this while running the interop tests.

--- integration/go/grpc/server/main.go
+++ integration/go/grpc/server/main.go
@@ -26,7 +26,7 @@ import (
 	"go.opencensus.io/tag"
 	"go.opencensus.io/trace"
 
-	pb "github.com/census-instrumentation/opencensus-experiments/integration/proto"
+	pb "github.com/census-ecosystem/opencensus-experiments/integration/proto"
 )
 
 type server int
