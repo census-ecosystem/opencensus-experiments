984c4017abee8335441b58b5836162150270c153
Peiqin Zhao <peiqinzhao@google.com>
2018-06-26 15:52:15 -0700

    Design HelloWorld protocol running on the same process without wire communication

--- go/iot/examples/ArduinoDriverTest.go
+++ go/iot/examples/ArduinoDriverTest.go
@@ -1,35 +1,32 @@
 package main
 
 import (
-	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/platforms/raspi"
-	"github.com/census-ecosystem/opencensus-experiments/go/iot/arduino"
-	"fmt"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/census-ecosystem/opencensus-experiments/go/iot/protocol"
 )
 
 func main() {
-	a := raspi.NewAdaptor()
-	adc := arduino.NewArduinoDriver(a)
-
-	work := func() {
-		adc.TransferAndWait('a')
-		adc.TransferAndWait(10)
-		a, err := adc.TransferAndWait(17)
-		b, err := adc.TransferAndWait(33)
-		c, err := adc.TransferAndWait(42)
-		d, err := adc.TransferAndWait(0)
-		fmt.Printf("A: %d, B: %d, C: %d, D: %d\n", a, b, c, d)
-		if err == nil{
-
-		}
-		select {}
+	test := &protocol.Test {
+		Label: proto.String("hello"),
+		Type:  proto.Int32(17),
+		Reps:  []int64{1, 2, 3},
 	}
-
-	robot := gobot.NewRobot("ArduinoDriver",
-		[]gobot.Connection{a},
-		[]gobot.Device{adc},
-		work,
-	)
-
-	robot.Start()
+	data, err := proto.Marshal(test)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	newTest := &protocol.Test{}
+	err = proto.Unmarshal(data, newTest)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	// Now test and newTest contain the same data.
+	if test.GetLabel() != newTest.GetLabel() {
+		log.Fatalf("data mismatch %q != %q", test.GetLabel(), newTest.GetLabel())
+	} else{
+		log.Print("Success!\n")
+	}
+	// etc.
 }
