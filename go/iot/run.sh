c72f87096bca34e2c9917eb6efdcd07b0c967eb8
Peiqin Zhao <peiqinzhao@google.com>
2018-07-09 16:37:45 -0700

    Collect Sensor Data and Export to the stackDriver (#45)
    Collect soundsensor, light sensor and temperature/humidity sensor data concurrently. Use tags to separate the two temperature sensors.

--- go/iot/run.sh
+++ go/iot/run.sh
@@ -7,5 +7,5 @@ fi
 CGO_ENABLED=1 GOARM=7 GOARCH=arm GOOS=linux CC=arm-linux-gnueabihf-gcc go build ./main.go
 sshpass -p $3 ssh $1@$2 "rm -rf /home/pi/main"
 sshpass -p $3 scp ./main $1@$2:~/
-sshpass -p $3 ssh $1@$2 "/home/pi/main"
+sshpass -p $3 ssh $1@$2 "/home/pi/main &"
 rm -rf ./main
