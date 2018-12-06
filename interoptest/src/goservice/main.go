20e328704610cf1518b2668001cd34b9ca5f35dd
rghetia <rghetia@yahoo.com>
2018-12-06 11:22:00 -0800

    add ocagent and jaeger. (#78)

--- interoptest/src/goservice/main.go
+++ interoptest/src/goservice/main.go
@@ -17,16 +17,63 @@ package main
 import (
   "net/http"
   "strings"
+  "log"
+
+  "github.com/gorilla/mux"
+  "go.opencensus.io/exporter/jaeger"
+  "contrib.go.opencensus.io/exporter/ocagent"
+  "go.opencensus.io/trace"
+  "go.opencensus.io/plugin/ochttp"
+  "go.opencensus.io/plugin/ochttp/propagation/tracecontext"
+
 )
+
+func registerJaegerExporter() {
+
+        // Register the Jaeger exporter to be able to retrieve
+        // the collected spans.
+        exporter, err := jaeger.NewExporter(jaeger.Options{
+                Endpoint: "http://traceui:14268",
+                Process: jaeger.Process{
+                        ServiceName: "goservice",
+                },
+        })
+        if err != nil {
+                log.Fatal(err)
+        }
+        trace.RegisterExporter(exporter)
+}
+
+func registerOcAgentExporter() {
+	oce, err := ocagent.NewExporter(ocagent.WithInsecure(), ocagent.WithAddress("ocagent:55678"))
+	if err != nil {
+		//log.Fatalf("Failed to create ocagent-exporter: %v", err)
+	}
+	trace.RegisterExporter(oce)
+	trace.ApplyConfig(trace.Config{
+		DefaultSampler: trace.AlwaysSample(),
+	})
+}
+
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
   message = strings.TrimPrefix(message, "/")
   message = "Hello, it is goservice " + message
   w.Write([]byte(message))
 }
+
 func main() {
-  http.HandleFunc("/", sayHello)
-  if err := http.ListenAndServe(":10201", nil); err != nil {
+        registerOcAgentExporter()
+        registerJaegerExporter()
+        r := mux.NewRouter()
+        r.HandleFunc("/", sayHello)
+
+        var handler http.Handler = r
+        handler = &ochttp.Handler{                     // add opencensus instrumentation
+                Handler:     handler,
+                Propagation: &tracecontext.HTTPFormat{}}
+
+  if err := http.ListenAndServe(":10201", handler); err != nil {
     panic(err)
   }
 }
