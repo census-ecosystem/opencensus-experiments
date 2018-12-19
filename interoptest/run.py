fa867040b961f5e2392381b6349aee263f1d2bd5
Chris Kleinknecht <libc@google.com>
2018-12-18 16:03:44 -0800

    Add timeout to script

--- interoptest/run.py
+++ interoptest/run.py
@@ -17,17 +17,20 @@
 Run the OpenCensus interop test and print the results.
 
 Usage:
-  run [--host=<host> [--port=<port>]] [--verbose]
+  run [--host=<host> [--port=<port>]] [--timeout=<timeout>] [--verbose]
   run (-h | --help)
 
 Options:
-  -h --help         Show help (this screen)
-  -v --verbose      Verbose output
-  -o --host=<host>  Test service host [default: localhost]
-  -p --port=<port>  Test service port [default: 10000]
+  -h --help               Show help (this screen)
+  -v --verbose            Verbose output
+  -t --timeout=<timeout>  Seconds to wait for service calls [default: 60]
+  -o --host=<host>        Test service host [default: localhost]
+  -p --port=<port>        Test service port [default: 10000]
 
 """
 
+from concurrent import futures
+from threading import Event
 import logging
 import sys
 import time
@@ -38,6 +41,7 @@ import grpc
 import interoperability_test_pb2 as pb2
 import interoperability_test_pb2_grpc as pb2_grpc
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARN)
 logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -46,17 +50,41 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 POLL_INTERVAL = 1
 
 
-def call_test(host, port):
+def call_test(host, port, executor, timeout=None):
     client = pb2_grpc.InteropTestServiceStub(
         channel=grpc.insecure_channel('{}:{}'.format(host, port)))
+
     logger.debug("Kicking off test run on %s:%s", host, port)
-    run_id = int(client.run(pb2.InteropRunRequest()).id)
+    run_future = executor.submit(client.run, pb2.InteropRunRequest())
+    start_time = time.time()
+    run_id = run_future.result(timeout=timeout).id
     logger.debug("Got test run ID %s", run_id)
-    response = client.result(pb2.InteropResultRequest(id=run_id))
-    while response is None or response.status.status == pb2.RUNNING:
-        logger.debug("Waiting for test run %s to finish...", run_id)
-        time.sleep(POLL_INTERVAL)
+
+    timeout_event = Event()
+
+    def poll_server():
         response = client.result(pb2.InteropResultRequest(id=run_id))
+        while response.status.status == pb2.RUNNING:
+            logger.debug("Waiting for test run %s to finish...", run_id)
+            if timeout_event.wait(POLL_INTERVAL):
+                raise futures.TimeoutError()
+            response = client.result(pb2.InteropResultRequest(id=run_id))
+        return response
+
+    result_future = executor.submit(poll_server)
+    if timeout is None:
+        time_left = None
+    else:
+        time_left = time.time() - start_time + timeout
+    try:
+        response = result_future.result(timeout=time_left)
+    except futures.TimeoutError:
+        timeout_event.set()
+        raise
+    except KeyboardInterrupt:
+        timeout_event.set()
+        raise
+
     logger.debug("Done test run %s", run_id)
     return response
 
@@ -67,7 +95,20 @@ def main():
         logger.setLevel(logging.DEBUG)
     host = args.get('--host', 'localhost')
     port = args.get('--port', 10000)
-    results = call_test(host, port)
+    timeout = int(args.get('--timeout', 60))
+    if timeout <= 0:
+        timeout = None
+    with futures.ThreadPoolExecutor(max_workers=2) as tpe:
+        try:
+            results = call_test(host, port, tpe, timeout=timeout)
+        except futures.TimeoutError as timeout_ex:
+            logger.fatal("Timed out waiting for response from test service")
+            logger.debug(timeout_ex)
+            sys.exit(-1)
+        except grpc.RpcError as rpc_ex:
+            logger.fatal("RPC error while calling test service")
+            logger.debug(rpc_ex)
+            sys.exit(-1)
     logger.debug("Results:")
     print(results)
 
