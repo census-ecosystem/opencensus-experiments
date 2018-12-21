3c65599e7c50fa94696dc290ff53c695994ddb43
Mayur Kale <mayurkale@google.com>
2018-12-21 12:23:18 -0800

    enable http instrumentation for nodeservice

--- interoptest/src/nodejsservice/server.js
+++ interoptest/src/nodejsservice/server.js
@@ -15,8 +15,16 @@
 const interop = require('./proto/interoperability_test_pb');
 const grpcServer = require('./src/testservice/grpc-server');
 const httpServer = require('./src/testservice/http-server');
+const httpPlugin = require('@opencensus/instrumentation-http');
+const tracing = require('@opencensus/nodejs');
+const {logger} = require('@opencensus/core');
+const http = require("http");
+const jaeger = require('@opencensus/exporter-jaeger');
 
 function main () {
+
+  enableHttpPlugin();
+
   // GRPC Server
   const grpcPort = interop.ServicePort.NODEJS_GRPC_BINARY_PROPAGATION_PORT;
   grpcServer.start(grpcPort);
@@ -29,6 +37,30 @@ function main () {
   //   httpServer.close();
   //   grpcServer.close();
   // }, 2000);
+}
+
+function enableHttpPlugin () {
+  // 1. Define service name, node version and jaeger options
+  const service = 'nodejsservice';
+  const version = process.versions.node;
+  const jaegerOptions = {
+    serviceName: service,
+    host: 'localhost',
+    port: 6832,
+    bufferTimeout: 10,
+    logger: logger.logger('debug')
+  };
+
+  // 2. Get the global singleton Tracer object
+  // 3. Configure 100% sample rate, otherwise, few traces will be sampled.
+  const tracer = tracing.start({samplingRate: 1}).tracer;
+
+  // 4. Configure exporter to export traces to Jaeger.
+  const exporter = new jaeger.JaegerTraceExporter(jaegerOptions);
+  tracer.registerSpanEventListener(exporter);
+
+  // 5. Enable HTTP plugin
+  httpPlugin.plugin.enable(http, tracer, version, null);
 }
 
 main();
