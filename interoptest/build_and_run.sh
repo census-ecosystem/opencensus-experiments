5abfa62396b16bdae53f2bd741a8b4a2955da02b
rahulpa <rahulpa@google.com>
2019-01-14 14:43:49 -0800

    run test using minikube on travis

--- interoptest/build_and_run.sh
+++ interoptest/build_and_run.sh
@@ -6,11 +6,10 @@ echo "Building containers.."
 skaffold build --profile travis-ci
 
 # Run the containers
-# [TODO:rghetia] invoke skaffold run after minikube issue is sorted out.
-# skaffold run
-# sleep 60
+skaffold run
+sleep 60
 
 # Run tests
-# cd ./src/testcontroller
-# sudo pip install -r requirements.txt
-# python run.py
+cd ./src/testcontroller
+sudo pip install -r requirements.txt
+python run.py
